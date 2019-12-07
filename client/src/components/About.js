48d7debf1878faab337ae0c193a84e0f446fc39a
Ivy Mageto <ivymageto@MirLeos-iPhone.localdomain>
2019-12-06 16:24:52 -0800

    styleed the contact page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -29,51 +29,62 @@ export default class About extends Component {
   render() {
     return (
       <>
-        <h1>Contact Us</h1>
-        <p>
-          We truly appreciate your questions and/or concerns. Please shoot us an
-          email, we would love to hear from you.
-        </p>
-        <div className="form-container">
-          <form onSubmit={this.handleSubmit}>
-            <div>
-              <div>
-                <label htmlFor="name">Name</label>
+        <div className="contact-container">
+          <h1 className="contact-title">Contact Us</h1>
+          <p className="contact-para">
+            We truly appreciate your questions and/or concerns. Please shoot us
+            an email and one of our staff members will get right back to you.
+          </p>
+          <div className="form-container">
+            <form onSubmit={this.handleSubmit}>
+              <div className="name-div">
+                <div>
+                  <label className="name-title" htmlFor="name">
+                    Name
+                  </label>
+                </div>
+                <div>
+                  <input
+                    className="name-input"
+                    type="text"
+                    name="name"
+                    onChange={event => this.handleChange(event)}
+                  />
+                </div>
               </div>
-              <div>
-                <input
-                  type="text"
-                  name="name"
-                  onChange={event => this.handleChange(event)}
-                />
+              <div className="email-div">
+                <div>
+                  <label className="email-title" htmlFor="email">
+                    Email
+                  </label>
+                </div>
+                <div>
+                  <input
+                    className="email-input"
+                    type="email"
+                    name="email"
+                    onChange={event => this.handleChange(event)}
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <div>
-                <label htmlFor="email">Email</label>
+              <div className="message-div">
+                <div>
+                  <label className="message-title" htmlFor="message">
+                    Message
+                  </label>
+                </div>
+                <div>
+                  <textarea
+                    className="message-input"
+                    type="textarea"
+                    name="message"
+                    onChange={event => this.handleChange(event)}
+                  />
+                </div>
               </div>
-              <div>
-                <input
-                  type="email"
-                  name="email"
-                  onChange={event => this.handleChange(event)}
-                />
-              </div>
-            </div>
-            <div>
-              <div>
-                <label htmlFor="message">Message</label>
-              </div>
-              <div>
-                <input
-                  type="textarea"
-                  name="message"
-                  onChange={event => this.handleChange(event)}
-                />
-              </div>
-            </div>
-            <button>SUBMIT</button>
-          </form>
+              <button className="contact-button">SUBMIT</button>
+            </form>
+          </div>
         </div>
       </>
     );
